8bf0325cfe3f0e6989645e4cfb8d5ba9933df5e9
Nikola Karaklic <nikolakaraklic@gmail.com>
2020-10-13 20:00:17 +0200

    Updates

---
+++ precache-manifest.cf23886919343ca09f4581a4f939734c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fabd4aabefa81a848afa8d5166e50a04",
+    "url": "/reactive-snake/index.html"
+  },
+  {
+    "revision": "802896b42a0a90a74c15",
+    "url": "/reactive-snake/static/css/main.823323cf.chunk.css"
+  },
+  {
+    "revision": "93464f9f61c2628f49ca",
+    "url": "/reactive-snake/static/js/2.5dfb4feb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactive-snake/static/js/2.5dfb4feb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "802896b42a0a90a74c15",
+    "url": "/reactive-snake/static/js/main.93d63818.chunk.js"
+  },
+  {
+    "revision": "894bd14128e0f65a688a",
+    "url": "/reactive-snake/static/js/runtime-main.8bc17c1e.js"
+  },
+  {
+    "revision": "0439cea2bf2e2df18afdd88b823df053",
+    "url": "/reactive-snake/static/media/background.0439cea2.png"
+  }
+]);
